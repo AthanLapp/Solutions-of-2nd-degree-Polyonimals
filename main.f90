bc34e25fadab63b150c8fa4aa091820b8031da72
Athanasios La <Athan.Lapp@gmail.com>
2020-09-16 14:36:32 +0300

    Add files via upload

---
+++ main.f90
@@ -0,0 +1,18 @@
+
+program poly_2
+    implicit none
+
+    real(16) :: ax, bx, cx, Xa, Xb, fkey
+    print*, 'Give me a, b, c of ax^2 + bx + c = 0 . :'
+    read*, ax, bx, cx
+    Xa = (-bx + sqrt(bx**2 - 4*ax*cx))/2*ax
+    Xb = (-bx + sqrt(bx**2 + 4*ax*cx))/2*ax
+    print *, "Given Polyonimal Solutions are:", Xa, Xb
+    print*, 'Press a key and ENTER to exit'
+    read*, fkey
+
+    print*, fkey
+
+
+end program
+
