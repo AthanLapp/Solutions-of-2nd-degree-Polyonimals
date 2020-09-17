5863e571779c44e9409b99c7fe4c6607685533b4
Athanasios La <Athan.Lapp@gmail.com>
2020-09-17 11:50:53 +0300

    Add files via upload

--- main.f90
+++ main.f90
@@ -6,7 +6,7 @@ program poly_2
     print*, 'Give me a, b, c of ax^2 + bx + c = 0 . :'
     read*, ax, bx, cx
     Xa = (-bx + sqrt(bx**2 - 4*ax*cx))/2*ax
-    Xb = (-bx + sqrt(bx**2 + 4*ax*cx))/2*ax
+    Xb = (-bx - sqrt(bx**2 - 4*ax*cx))/2*ax
     print *, "Given Polyonimal Solutions are:", Xa, Xb
     print*, 'Press a key and ENTER to exit'
     read*, fkey
